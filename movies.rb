bcb953be318187d416bb0ac1d0a3f3aee8e18032
Jozef <jozefw@sbcglobal.net>
2014-04-21 08:58:26 -0700

    commit

--- movies.rb
+++ movies.rb
@@ -10,7 +10,36 @@ configure do
   set :views, File.join(root,'views')
 end
 
+# on this page show erb to / page
+
+
 get '/' do
+	erb :index
 end
 
+get '/search' do
+  movie = params[:movie]
+  # goes to search url and gets the movie 
+  results = Typhoeus.get("http://www.omdbapi.com", :params => {:s => movie })
+  # gets the results of the search 
+  imdb_data = JSON.parse(results.body)
+  # saves just the parsed body as a string
+  @movie_result = imdb_data["Search"]
+  erb :search
+end
 
+get '/show/:imdbID' do
+  search = params[:imdbID]
+  results = Typhoeus.get("http://www.omdbapi.com", :params => {:i => search})
+ @imdb_data = JSON.parse(results.body)
+erb :show
+end
+
+# if search == "" || search == nil
+# return "This movie does not exist"
+# end
+
+# # if results != do
+# # "Movie Does not Exist"
+
+# end
--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -1,6 +1,8 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe "Sinatra Movie Application With OMDB API" do
+	def app
+		Sinatra::Application
 
   describe "Home page" do
     it "should allow accessing the home page" do
@@ -9,5 +11,20 @@ describe "Sinatra Movie Application With OMDB API" do
     end
   end
 
+describe "Search Results" do
+    it "should return results for sharknado" do
+      get '/results?movie=sharknado'
+      last_response.should be_ok
+    end
+it "should return the year sharknado was made" do
+      get '/results?movie=sharknado'
+      last_response.should be_ok
+      expect(last_response.body).to include("2013")
+    end
   
+   end
+    it "should return the sharknado poster" do
+      get '/poster/tt2724064'
+      expect(last_response.body).to include("http://ia.media-imdb.com/images/M/MV5BOTE2OTk4MTQzNV5BMl5BanBnXkFtZTcwODUxOTM3OQ@@._V1_SX300.jpg")
+    end
 end
